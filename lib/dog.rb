581e1f892f0baa9ee47f2094c4bd385864118a47
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-21 05:11:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Dog 
+  def bark 
+    puts "Woof!"
+  end 
+  def name=(dog_name)
+    this_is_the_name = dog_name
+  end
+  def name
+    puts this_is_the_name
+  end
+end
